f9dfe976561012cedd2e219fdf767a9bca2f528d
Norvert John <noveth2012@gmail.com>
2015-09-06 23:34:27 +0800

    add database configurations

--- services/db.js
+++ services/db.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+//SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'DBName'
 var pool      =    mysql.createPool({
     connectionLimit : 100, //important
     host     : 'localhost',
@@ -9,6 +10,16 @@ var pool      =    mysql.createPool({
 });
 
 console.log('Connected to db');
+pool.getConnection(function(err,connection){
+    if (err) {
+      console.log("Error in connection database");
+        if (err.errno === 1049) {
+            // create new db and table
+            createMysqlDB(connection);
+        }
+    }
+});
+
 var db = {
     get: function(data, callback){
           pool.getConnection(function(err,connection){
@@ -83,5 +94,60 @@ function buildQuery(data){
     return query;
 }
 
+function createMysqlDB()
+{
+  var client = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'rssfeed',
+      password : 'rssfeed'
+  });
+  client.connect();
+
+  client.query('CREATE DATABASE rssfeedmaster', function(err, results) {
+		if (err) {
+			console.log('error on creating database');
+      return false;
+		} else {
+      var tbl_articles = "CREATE TABLE tbl_articles ( id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255) DEFAULT NULL, link VARCHAR(255) DEFAULT NULL, url VARCHAR(255) DEFAULT NULL,likes INT(11) DEFAULT 0 )";
+      var tbl_user = "CREATE TABLE tbl_user ( id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, cookie_id VARCHAR(255) DEFAULT NULL, name VARCHAR(255) DEFAULT NULL, email VARCHAR(255) DEFAULT NULL )";
+      var tbl_article_has_likes = "CREATE TABLE tbl_article_has_likes ( id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, article_id INT(11) DEFAULT NULL, user_id INT(11) DEFAULT NULL)";
+
+      client.query('USE rssfeedmaster', function(err, results){
+          if (err) {
+              console.log('ERROR USING RSSFEEDMASTER DATABASE');
+          }
+      });
+      client.query(tbl_articles, function(err, results) {
+          if (err) {
+            console.log("ERROR ON CREATING TBL_ARTICLES "  + err.message);
+          }
+      });
+
+      client.query(tbl_user, function(err, results) {
+          if (err) {
+            console.log("ERROR ON CREATING TBL_USER "  + err.message);
+          }
+      });
+
+      client.query(tbl_article_has_likes, function(err, results) {
+          if (err) {
+            console.log("ERROR ON CREATING TBL_ARTICLE_HAS_LIKES " + err.message);
+          }
+      });
+
+      //recreate pool
+      pool = mysql.createPool({
+          connectionLimit : 100, //important
+          host     : 'localhost',
+          user     : 'rssfeed',
+          password : 'rssfeed',
+          database : 'rssfeedmaster',
+          debug    :  false
+      });
+      return true;
+    }
+
+	});
+}
 
 module.exports = db;
